6d7314f1289a33c5c9f7ff7697cf0b3254ac0b2f
QuentinSeillon <quentin.seillon@gmail.com>
2025-04-07 18:56:50 +0200

    Mise en place de Swagger

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,17 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from 'src/dto/register.dto';
 import { LoginDto } from 'src/dto/login.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('register')
+    @ApiOperation({ summary: 'Inscription d\'un utilisateur' })
     async register(@Body() registerDto: RegisterDto) {
         return this.authService.register(registerDto);
     }
 
     @Post('login')
+    @ApiOperation({ summary: 'Connexion d\'un utilisateur'})
     async login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
     }
--- server/src/dto/register.dto.ts
+++ server/src/dto/register.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'test@gmail.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'Password'})
   @IsNotEmpty()
   @MinLength(4)
   password: string;
