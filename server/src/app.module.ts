908766b4c4f2d13eab6f75439a26f9e296182789
QuentinSeillon <quentin.seillon@gmail.com>
2025-04-10 11:24:54 +0200

    Déploiement sur Render

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -28,6 +28,9 @@ import { Reservation } from './reservation/reservation.entity';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
       entities: [
         User,
         Reservation
